6ed2932d46b01b7d1772da82221cdb578fda030a
RK00333040 <RK00333040@INHYICLP00280.TechMahindra.com>
2020-07-02 21:22:53 +0530

    This is the commit

--- functional-programming-with-java/src/programming/FP01Functional.java
+++ functional-programming-with-java/src/programming/FP01Functional.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class FP01Functional {
 
 	public static void main(String[] args) {
-		
-		List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 //		printAllNumbersInListFunctional(numbers);
 		printAllEvenNumbersInListFunctional(numbers);
 
@@ -15,20 +15,19 @@ public class FP01Functional {
 //	private static void print(int number) {
 //		System.out.println(number);
 //	}
-	
+
 	private static boolean isEven(int number) {
-		return number%2==0;
+		return number % 2 == 0;
 	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-		numbers.stream()
-			.forEach(System.out::println);//Method Reference
+		numbers.stream().forEach(System.out::println);// Method Reference
 	}
-	
+
 	private static void printAllEvenNumbersInListFunctional(List<Integer> numbers) {
-		numbers.stream()
-			.filter(FP01Functional::isEven)//Filter is only allow even number
-			.forEach(System.out::println);//Method Reference
+		numbers.stream().filter(FP01Functional::isEven)// Filter is only allow even number
+				.forEach(System.out::println);// Method Reference
+
 	}
 
 }
